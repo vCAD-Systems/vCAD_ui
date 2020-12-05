56456e9a9fd3414e456d112766e9000089963f91
Lars H <30929430+ZockerBoysHD@users.noreply.github.com>
2020-12-05 22:52:51 +0100

    Fixxed Animations
    I fixxed the animation Part to actually show an Opening a tablet animation and attaching a real Tablet Prop to the Player (working reliable!)

--- client.lua
+++ client.lua
@@ -13,6 +13,7 @@ local Keys = {
 ESX = nil
 local tabEnabled, tabLoaded, isDead, lastOpend, site, subSite = false, false, false, 0, 'cop', 'tab'
 local PlayerData = {}
+local tab
 
 Citizen.CreateThread(function()
 	while ESX == nil do
@@ -74,13 +75,12 @@ function REQUEST_NUI_FOCUS(bool)
 			SetNuiFocus(bool, bool)
 			SetCurrentPedWeapon(PlayerPed, GetHashKey('WEAPON_UNARMED'), true)
 			
-			RequestAnimDict('anim_heist@arcade_combined@')
-
-			while not HasAnimDictLoaded('anim_heist@arcade_combined@') do
-				Citizen.Wait(1)
+			RequestAnimDict("amb@world_human_seat_wall_tablet@female@base")
+			while not HasAnimDictLoaded("amb@world_human_seat_wall_tablet@female@base") do
+			  Citizen.Wait(1)
 			end
-
-                        TaskPlayAnim(PlayerPed, 'anim_heist@arcade_combined@', 'world_human_stand_mobile@_male@_text@_idle_a', 8.0, -8.0, -1, 16, 0, false, false, false)
+			attachObject()
+                        TaskPlayAnim(PlayerPed, "amb@world_human_seat_wall_tablet@female@base", "base" ,8.0, -8.0, -1, 50, 0, false, false, false)
 		end
 	else
        	 	SendNUIMessage({hidetab = true})
@@ -88,8 +88,14 @@ function REQUEST_NUI_FOCUS(bool)
 		
 		if Config.Animation == true then
 			ClearPedTasks(PlayerPed)
+			DeleteObject(tab)
 		end
 	end
+end
+
+function attachObject()
+	tab = CreateObject(GetHashKey("prop_cs_tablet"), 0, 0, 0, true, true, true)
+	AttachEntityToEntity(tab, GetPlayerPed(-1), GetPedBoneIndex(GetPlayerPed(-1), 57005), 0.17, 0.10, -0.13, 20.0, 180.0, 180.0, true, true, false, true, 1, true)
 end
 
 RegisterNUICallback("tablet-bus", function(data)
@@ -111,6 +117,7 @@ AddEventHandler('onResourceStop', function(resource)
 			
 			if Config.Animation == true then
 				ClearPedTasks(PlayerPedId())
+				DeleteObject(tab)
 			end
 		end
 	end
